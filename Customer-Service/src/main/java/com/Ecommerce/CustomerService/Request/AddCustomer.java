dc07e4102b2d95ed076ab31b6d852d4e97b14a97
Benjiealcontin <Benjiealcontin23@gmail.com>
2023-09-05 10:22:55 +0800

    I create Delete Request in Customer and Validation

--- Customer-Service/src/main/java/com/Ecommerce/CustomerService/Request/AddCustomer.java
+++ Customer-Service/src/main/java/com/Ecommerce/CustomerService/Request/AddCustomer.java
@@ -1,27 +1,43 @@
 package com.Ecommerce.CustomerService.Request;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 @Data
 @AllArgsConstructor
 public class AddCustomer {
+    @NotBlank(message = "Username is required")
     private String username;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email format")
     private String email;
+
     private boolean enabled;
+
+    @NotBlank(message = "First name is required")
     private String firstName;
+
+    @NotBlank(message = "Last name is required")
     private String lastName;
-    private Map<String, Object> attributes;
+
+    private Address attributes;
+
     private List<String> groups;
+
+    @Valid
     private List<Credentials> credentials;
 
     public AddCustomer() {
         this.groups = new ArrayList<>();
         this.groups.add("Consumer"); // Set the default value
     }
+
+
 }
--- Customer-Service/src/main/java/com/Ecommerce/CustomerService/Request/Credentials.java
+++ Customer-Service/src/main/java/com/Ecommerce/CustomerService/Request/Credentials.java
@@ -1,5 +1,8 @@
 package com.Ecommerce.CustomerService.Request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,7 +11,13 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Credentials {
+    @NotBlank(message = "Type is required")
     private String type;
+
+    @NotBlank(message = "Value is required")
+    @Size(min = 8, message = "Value must be at least 8 characters long")
     private String value;
+
+    @NotNull(message = "Temporary is required")
     private boolean temporary;
 }
